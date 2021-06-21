bb1ed130d9159cf2704472007205b255bbc5e8ad
Connor <connorchenderson9@gmail.com>
2021-06-21 12:34:02 -0400

    set up models

---
+++ app/models/user_security.py
@@ -0,0 +1,13 @@
+from .db import db
+from sqlalchemy.orm import relationship
+
+class UserSecurity(db.Model):
+    __tablename__ = "user_securities"
+
+    id = db.Column(db.Integer, primary_key = True)
+    relation = db.Column(db.String(30), nullable=False)
+    userId = db.Column(db.Integer, db.ForeignKey("users.id"))
+    securityId = db.Column(db.Integer, db.ForeignKey("securities.id"))
+
+    users = relationship("User", back_populates="user_security")
+    securities = relationship("Security", back_populates="user_security")
