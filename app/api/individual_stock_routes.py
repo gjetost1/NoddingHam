ac2bd08c28e0a1c7f8a2c355445da021b5512e21
becklesoweno@gmail.com <becklesoweno@gmail.com>
2021-06-21 19:09:29 -0400

    Ticker Query

--- app/api/individual_stock_routes.py
+++ app/api/individual_stock_routes.py
@@ -3,6 +3,6 @@ from app.models import db
 
 individual_stock_routes = Blueprint("individual-stock", __name__)
 
-@individual_stock_routes.route('/securities/${securities.ticker}')
+@individual_stock_routes.route('/securities/<ticker>')
 def individual_stock():
-    
+    security = Securities.query.filter()
