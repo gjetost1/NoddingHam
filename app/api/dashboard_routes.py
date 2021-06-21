5dbfb47e7bc1f1d91be90d15d6cda85663772d2e
Connor <connorchenderson9@gmail.com>
2021-06-21 19:53:55 -0400

    working api call

--- app/api/dashboard_routes.py
+++ app/api/dashboard_routes.py
@@ -1,11 +1,10 @@
 from flask import Blueprint, jsonify
-# import alpaca_trade_api as tradeapi
 import os
+from datetime import datetime, timezone, timedelta
+from alpaca_trade_api.rest import REST, TimeFrame
 
 dashboard_routes = Blueprint("dashboard", __name__)
 
-# alpaca = tradeapi.REST(key_id=os.environ.get("APCA_API_KEY_ID"),secret_key=os.environ.get("APCA_API_SECRET_KEY"))
-from alpaca_trade_api.rest import REST
 
 api = REST(os.environ.get("APCA_API_KEY_ID"), os.environ.get("APCA_API_SECRET_KEY"), api_version='v2')
 # api = REST("AKAH50HMXHBHFPJF6G4R", "KE0Uoia7scWUpFywwVHcWNYVXK80kxkzDC1W7dDE", api_version='v2')
@@ -13,7 +12,22 @@ api = REST(os.environ.get("APCA_API_KEY_ID"), os.environ.get("APCA_API_SECRET_KE
 @dashboard_routes.route("/")
 def home_page():
     # get 10 stocks
-    quotes_call = api.get_quotes("AAPL", "2021-02-08", "2021-02-08", limit=10).df
-    quotes_data = quotes_call.to_dict()
+    # quotes_call = api.get_quotes("AAPL", "2021-02-01", "2021-02-08", limit=1000).df
 
-    return quotes_data
+    tickers = ["AAPL", "AMZN", "GOOG"]
+    limit = 365*2
+
+    # RFC3339 date format
+    end_date = datetime.now(timezone.utc).astimezone()
+    delta = timedelta(days=365*2)
+    start_date = end_date-delta
+
+    end_date.isoformat()
+    start_date.isoformat()
+    quotes_call = api.get_barset(tickers,
+                                 "day",
+                                 limit,
+                                 start_date,
+                                 end_date)
+
+    return quotes_call
