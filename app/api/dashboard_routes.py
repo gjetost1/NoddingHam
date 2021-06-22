82b567ec02395c4a15fb69359a8a2a499e1f1537
Connor <connorchenderson9@gmail.com>
2021-06-22 11:58:59 -0400

    jsonified backend returns

--- app/api/dashboard_routes.py
+++ app/api/dashboard_routes.py
@@ -1,40 +1,14 @@
-from flask import Blueprint, jsonify
-# import os
-# from datetime import datetime, timezone, timedelta
-# from alpaca_trade_api.rest import REST, TimeFrame
-from app.utils import get_historical_data
+from flask import Blueprint
+from app.utils import get_historical_data, remap_keys
 
 dashboard_routes = Blueprint("dashboard", __name__)
 
 
 @dashboard_routes.route("/")
 def home_page():
-    # get 10 stocks
-    # quotes_call = api.get_quotes("AAPL", "2021-02-01", "2021-02-08", limit=1000).df
-
     tickers = ["AAPL", "AMZN", "GOOG"]
-    # limit = 365*2
-
-    # # RFC3339 date format
-    # end_date = datetime.now(timezone.utc).astimezone()
-    # delta = timedelta(days=365*2)
-    # start_date = end_date-delta
-
-    # end_date.isoformat()
-    # start_date.isoformat()
-    # historical_data = api.get_barset(tickers,
-    #                              "day",
-    #                              limit,
-    #                              start_date,
-    #                              end_date)
 
     historical_data = get_historical_data(tickers).df
-    historical_data_dict = historical_data.to_dict()
-    res = dict()
-    for key, value in historical_data_dict.items():
-        new_key = f"{key[0]}-{key[1]}"
-        res[new_key] = value
-
-    print(res)
+    res = remap_keys(historical_data.to_dict())
 
     return res
--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -2,8 +2,10 @@ from datetime import datetime, timezone, timedelta
 from alpaca_trade_api.rest import REST, TimeFrame
 import os
 
-api = REST(os.environ.get("APCA_API_KEY_ID"), os.environ.get("APCA_API_SECRET_KEY"), api_version='v2')
-# api = REST("AKAH50HMXHBHFPJF6G4R", "KE0Uoia7scWUpFywwVHcWNYVXK80kxkzDC1W7dDE", api_version='v2')
+api = REST(os.environ.get("APCA_API_KEY_ID"),
+           os.environ.get("APCA_API_SECRET_KEY"),
+           api_version='v2')
+
 
 def get_historical_data(tickers):
     # Maximum number of days per bar
@@ -23,3 +25,19 @@ def get_historical_data(tickers):
                                      end_date)
 
     return historical_data
+
+
+def remap_keys(mapping):
+    json_mapping = dict()
+
+    for parent_key, parent_value in mapping.items():
+        new_parent_key = f"{parent_key[0]}-{parent_key[1]}"
+        new_parent_value = dict()
+
+        for child_key, child_value in parent_value.items():
+            new_child_key = child_key.strftime("%d-%b-%Y (%H:%M:%S.%f)")
+            new_parent_value[new_child_key] = child_value
+
+        json_mapping[new_parent_key] = new_parent_value
+
+    return json_mapping
