572fe7434ab9d2b35006a1f6a543316b3a06dea8
becklesoweno@gmail.com <becklesoweno@gmail.com>
2021-06-25 14:14:18 -0400

    Button Styling Template

--- react-app/src/components/Portfolio/postToPortfolio.js
+++ react-app/src/components/Portfolio/postToPortfolio.js
@@ -12,7 +12,7 @@ const onAdd = (e) => {
     return (
         <button
         type="button" id='addbutton'
-        className="inline-flex items-center w-0.5 text-xs px-2 py-1 mx-5 border border-pink-500 rounded-md shadow-sm font-medium text-pink-500 bg-white hover:bg-pink-500 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-white no-underline"
+        className="inline-flex items-center w-0.5 text-xs px-2 py-1 mx-5 mt-8 border border-pink-500 rounded-md shadow-sm font-medium text-pink-500 bg-white hover:bg-pink-500 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-white no-underline"
         onClick={onAdd}
         >
         + Portfolio
