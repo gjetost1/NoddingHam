46e2e691a049265ea7c2eb2c9223c4e6dd14d815
becklesoweno@gmail.com <becklesoweno@gmail.com>
2021-06-25 13:39:55 -0400

    Initial Button Styling

--- react-app/src/components/Watchlist/deleteFromWatchlist.js
+++ react-app/src/components/Watchlist/deleteFromWatchlist.js
@@ -13,7 +13,7 @@ const onClick = (e) => {
     return (
         <button
         type="button" id='addbutton'
-        className="inline-flex items-center px-2.5 py-1.5 border border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        className="inline-flex items-center w-0.5 text-xs px-2 py-1 mx-5 border border-transparent rounded-md shadow-sm font-medium text-white bg-pink-500 hover:bg-pink-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-white no-underline"
         onClick={onClick}
         >
         - Watchlist
